4cbd38bb73b5bf24435879863bde56abe85a9dc9
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-29 17:45:58 +0800

    1.05 root GET returns single HTML page

--- exercises/part-1/todo-app/src/index.ts
+++ exercises/part-1/todo-app/src/index.ts
@@ -1,8 +1,19 @@
 import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
 const app = express();
 app.use(express.json());
+
+const simplePageHtml = `<!DOCTYPE html>
+<html lang="en">
+<head></head>
+<body>
+  <h1>Hello World!</h1>
+</body>
+</html>
+`
+
+app.get('/', (_req, res) => {
+  res.send(simplePageHtml);
+})
 
 const PORT = process.env.PORT;
 
