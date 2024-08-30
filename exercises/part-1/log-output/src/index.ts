3d3a79513f3c2118ba786aca76e25efe231cddb8
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-08-30 14:53:47 +0800

    1.07 log output now an endpoint

--- exercises/part-1/log-output/src/index.ts
+++ exercises/part-1/log-output/src/index.ts
@@ -1,3 +1,11 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+let logOutput = '';
+
 function generateRandomString() {
   return (
     Math.random().toString(36).substring(2, 15) +
@@ -8,5 +16,15 @@ function generateRandomString() {
 setInterval(() => {
   const timestamp = new Date().toISOString();
   const randomString = generateRandomString();
-  console.log(`${timestamp}: ${randomString}`);
+  logOutput = `[${timestamp}] ${randomString}`;
 }, 5000);
+
+app.get('/', (_req, res) => {
+  res.send({
+    logOutput,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
