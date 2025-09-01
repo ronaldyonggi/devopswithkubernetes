d465b31dc997a78471439a968e49fe9e930dcca1
Ronald Yonggi <ronaldyonggi@gmail.com>
2025-09-01 17:16:48 +0800

    exercise 1.7 log-output now uses ingress

--- log_output/index.js
+++ log_output/index.js
@@ -1,10 +1,21 @@
-const crypto = require('crypto')
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+
+// Random string is created once when the app starts
 const randomString = crypto.randomBytes(16).toString('hex');
+const PORT = process.env.PORT || 3001;
 
-const logOutput = () => {
+// This route handler is executed for every GET to '/'
+app.get('/', (req, res) => {
+    // A new timestamp is created for each request
     const timestamp = new Date().toISOString();
-    console.log(`${timestamp} - ${randomString}`);                      
-}
+    const response = `${timestamp} - ${randomString}`;
+    console.log('Sending response:', response);
+    res.send(response);
+})
 
-console.log('Application starting...');
-setInterval(logOutput, 5000);
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
+
