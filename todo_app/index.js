13073e94067d10c4439a74eeaaa23bb5a7aa34e8
Ronald Yonggi <ronaldyonggi@gmail.com>
2025-09-01 13:19:48 +0800

    exercise 1.5 add a response for / url

--- todo_app/index.js
+++ todo_app/index.js
@@ -3,6 +3,10 @@
 
  const PORT = process.env.PORT || 3000;
 
+ app.get('/', (req, res) => {
+    res.send('Respond for exercise 1.5!');
+ })
+
  app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
  })
