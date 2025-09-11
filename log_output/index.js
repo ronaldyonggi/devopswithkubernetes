1ecb019b070922e60589e1c7f727cae7e9b1b3f0
Ronald Yonggi <ronaldyonggi@gmail.com>
2025-09-11 16:29:22 +0800

    complete exercise 2.1 connecting pods

--- log_output/index.js
+++ log_output/index.js
@@ -1,31 +1,31 @@
 const express = require("express");
 const crypto = require("crypto");
-const fs = require("fs");
-const path = require("path");
 const app = express();
 
 // Random string is created once when the app starts
 const randomString = crypto.randomBytes(16).toString("hex");
 const PORT = process.env.PORT || 3001;
-const FILE_DIR = "/usr/src/app/files";
-const FILE_PATH = path.join(FILE_DIR, "pongs.txt");
+
+const PING_PONG_URL = "http://ping-pong-svc:2347/pingpong";
 
 // This route handler is executed for every GET to '/'
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   // A new timestamp is created for each request
   const timestamp = new Date().toISOString();
   let pongCount = 0;
 
-  // Try to read the pong count from the shared file
   try {
-    const data = fs.readFileSync(FILE_PATH, "utf8");
-    pongCount = parseInt(data) || 0;
-  } catch (error) {
-    console.log("Pong file not found, starting counter from 0");
+    // Make the request to ping-pong service
+    const response = await fetch(PING_PONG_URL);
+
+    // Get the ext content from the response
+    pongCount = await response.text();
+  } catch (err) {
+    console.error("Error fetching pongs:", err.message);
   }
 
-  const response = `${timestamp}: ${randomString}\nPing / Pongs: ${pongCount}`;
-  res.type('text/plain').send(response);
+  const responseString = `${timestamp}: ${randomString}\nPing / Pongs: ${pongCount}`;
+  res.type("text/plain").send(responseString);
 });
 
 app.listen(PORT, () => {
--- ping_pong/index.js
+++ ping_pong/index.js
@@ -1,31 +1,13 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3002;
 
-const FILE_DIR = "/usr/src/app/files";
-const FILE_PATH = path.join(FILE_DIR, "pongs.txt");
-
-// Ensure the directory exists
-fs.mkdirSync(path.dirname(FILE_PATH), { recursive: true });
+let counter = 0;
 
 app.get("/pingpong", (req, res) => {
-  let counter = 0;
-  // Read the current count from the file
-  try {
-    const data = fs.readFileSync(FILE_PATH, 'utf8');
-    counter = parseInt(data) || 0;
-  } catch(error) {
-    console.log('Pong file not found, starting counter from 0');
-  }
-
   counter++;
-
-  // Write the new count back to the file
-  fs.writeFileSync(FILE_PATH, counter.toString());
-  res.send(`pong ${counter}`);
+  res.send(counter.toString());
 });
 
 app.listen(PORT, () => {
