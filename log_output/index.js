3551fcd8a55fb52fde5ab181713cf848e97de956
Ronald Yonggi <ronaldyonggi@gmail.com>
2025-09-28 14:59:04 +0800

    complete exercise 2.5 configmaps

--- log_output/index.js
+++ log_output/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
 
 // Random string is created once when the app starts
 const randomString = crypto.randomBytes(16).toString("hex");
+const MESSAGE = process.env.MESSAGE;
 const PORT = process.env.PORT || 3001;
+
+const INFORMATION_FILE_PATH = path.join("/etc/config", "information.txt");
 
 const PING_PONG_URL = "http://ping-pong-svc:2347/pingpong";
 
@@ -13,6 +19,13 @@ app.get("/", async (req, res) => {
   // A new timestamp is created for each request
   const timestamp = new Date().toISOString();
   let pongCount = 0;
+
+  let fileContent = "";
+
+  // Check if the information file exists
+  if (fs.existsSync(INFORMATION_FILE_PATH)) {
+    fileContent = fs.readFileSync(INFORMATION_FILE_PATH, "utf-8");
+  }
 
   try {
     // Make the request to ping-pong service
@@ -24,7 +37,11 @@ app.get("/", async (req, res) => {
     console.error("Error fetching pongs:", err.message);
   }
 
-  const responseString = `${timestamp}: ${randomString}\nPing / Pongs: ${pongCount}`;
+  const responseString = `
+  file content: ${fileContent}\n
+  env variable: MESSAGE=${MESSAGE}\n
+  ${timestamp}: ${randomString}\n
+  Ping / Pongs: ${pongCount}`;
   res.type("text/plain").send(responseString);
 });
 
